644b860e62cca10d9fd548fc819d97d5c3fa65cd
odinsam <123679@qq.com>
2024-01-08 06:45:09 +0800

    no message

--- KunLun.Api/Program.cs
+++ KunLun.Api/Program.cs
@@ -1,22 +1,34 @@
-var builder = WebApplication.CreateBuilder(args);
+using AspNetCoreRateLimit;
+using Cola.ColaMiddleware.ColaIpRateLimit;
+using Cola.ColaMiddleware.ColaSwagger;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("appsettings.json");
+var config = builder.Configuration;
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// builder.Services.AddSwaggerGen();
+builder.Services.AddColaSwagger(config);
+builder.Services.AddSingletonColaIpRateLimit(config);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // app.UseSwagger();
+    // app.UseSwaggerUI();
+    app.UseColaSwagger("/swagger/v1/swagger.json", "WebApi V1",null);
 }
 
+
+
 app.UseHttpsRedirection();
+
+app.UseIpRateLimiting();
 
 app.UseAuthorization();
 
